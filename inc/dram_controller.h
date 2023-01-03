ChampSim/ChampSim

Removed is_valid<PACKET>

inc/dram_controller.h
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <cmath>
 #include <limits>
+#include <optional>
 
 #include "champsim_constants.h"
 #include "memory_class.h"
@@ -19,7 +20,7 @@ struct dram_stats {
 };
 
 struct DRAM_CHANNEL {
-  using queue_type = std::vector<PACKET>;
+  using queue_type = std::vector<std::optional<PACKET>>;
   queue_type WQ{DRAM_WQ_SIZE}, RQ{DRAM_RQ_SIZE};
 
   struct BANK_REQUEST {

yanxinzhangcs/ChampSim_checkpoint

Conflicts:
	inc/dram_controller.h
	inc/memory_class.h
	src/cache.cc
	src/cache_queues.cc
	src/dram_controller.cc
	src/ptw.cc

inc/dram_controller.h
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <cmath>
 #include <limits>
+#include <optional>
 
 #include "champsim_constants.h"
 #include "channel.h"
@@ -21,7 +22,7 @@ struct dram_stats {
 struct DRAM_CHANNEL {
   using value_type = typename champsim::channel::request_type;
   using response_type = typename champsim::channel::response_type;
-  using queue_type = std::vector<value_type>;
+  using queue_type = std::vector<std::optional<value_type>>;
   queue_type WQ{DRAM_WQ_SIZE}, RQ{DRAM_RQ_SIZE};
 
   struct BANK_REQUEST {